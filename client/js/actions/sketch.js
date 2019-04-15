e631f717ebb9067a0593ae6b598ddd32a87c00eb
Mike Skalandunas <mskalandunas@gmail.com>
2019-04-15 09:46:52 -0700

    start on note entry

---
+++ client/js/actions/sketch.js
@@ -0,0 +1,11 @@
+import { ADD_NOTE, REMOVE_NOTE } from '../constants/actions';
+
+export const addNote = noteDetails => {
+    type: ADD_NOTE,
+    noteDetails
+};
+
+export const removeNote = noteDetails => {
+    type: REMOVE_NOTE,
+    noteDetails
+};
---
+++ client/js/components/Staff.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+
+class Staff extends Component {
+    render() {
+
+    }
+}
+
+const mapDispatchToProps = {
+
+};
+
+const mapStateToProps = state => {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Staff);
