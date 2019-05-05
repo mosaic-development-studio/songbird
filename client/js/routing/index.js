b162ceb212891f4b8232174f4660dd9619ed4f48
Mike Skalandunas <mskalandunas@gmail.com>
2019-05-05 16:40:11 -0700

    [routing]: move transforms to dedicated file

--- client/js/routing/index.js
+++ client/js/routing/index.js
@@ -1,95 +1,37 @@
-import { BASE64_REGEX } from '../constants/constants';
-
-const convertBase64StringToState = base64String => JSON.parse(atob(base64String));
+import {
+    basicURIPathValidators,
+    convertBase64StringToState,
+    isBase64Valid,
+    isStateValid,
+    isValidJSONString
+} from './validators';
+import { transformStateForHash, transformTrimmedStatetoState } from '../transformers';
 
 export const convertStateToBase64String = state => btoa(JSON.stringify(state));
 
 const getLocationPathname = () => window.location.pathname;
 
-const isBase64Valid = base64Data => BASE64_REGEX.test(base64Data);
-
-const isValidJSONString = string => {
-    try {
-        convertBase64StringToState(string);
-    }
-
-    catch(e) {
-        return false;
-    }
-
-    return true;
-};
-
-const checkValueAgainstType = (value, type) => {
-    if (type === 'array') {
-        return Array.isArray(value);
-    }
-
-    return typeof value === type;
-};
-
-const isStateValid = parsedJSON => {
-    const keys = Object.keys(parsedJSON);
-
-    return keys.reduce((isValid, key) => {
-        switch(key) {
-            case 'beatValue':
-            case 'beatsPerMeasure':
-            case 'tempo':
-                return isValid && checkValueAgainstType(parsedJSON[key], 'number');
-            case 'sketch':
-                return isValid
-                    && checkValueAgainstType(parsedJSON[key], 'array')
-                    && parsedJSON[key].reduce((indexIsValidNote, noteDetails) => {
-                        return indexIsValidNote && Object.keys(noteDetails).reduce(
-                            (keyIsValid, prop) => {
-                                switch(prop) {
-                                    case 'bar':
-                                    case 'subdivision':
-                                        return keyIsValid && checkValueAgainstType(
-                                            noteDetails[prop],
-                                            'number'
-                                        );
-                                    case 'pitch':
-                                        return keyIsValid && checkValueAgainstType(
-                                            noteDetails[prop],
-                                            'string'
-                                        );
-                                    default:
-                                        return false;
-                                }
-                            }, true
-                        ) && [
-                            'bar',
-                            'pitch',
-                            'subdivision'
-                        ].every(noteProp => ['bar', 'pitch', 'subdivision'].includes(noteProp));
-                    }, true);
-            default:
-                return false;
-        }
-    }, true) && [
-        'beatValue',
-        'beatsPerMeasure',
-        'sketch',
-        'tempo'
-    ].every(prop => keys.includes(prop));
-};
-
 export const instantiateApplicationStateFromPathname = defaultState => {
     const uriPathStringWithoutSlash = getLocationPathname().replace('/', '');
 
-    if (
-        uriPathStringWithoutSlash.length
-            && isBase64Valid(uriPathStringWithoutSlash)
-            && isValidJSONString(uriPathStringWithoutSlash)
-    ) {
+    if (basicURIPathValidators(uriPathStringWithoutSlash, [
+        isBase64Valid, isValidJSONString
+    ])) {
         const potentialState = convertBase64StringToState(uriPathStringWithoutSlash);
 
         if (isStateValid(potentialState)) {
-            return potentialState;
+            return transformTrimmedStatetoState(potentialState, defaultState);
         }
     }
 
     return defaultState;
+};
+
+export const stateToHashMiddleware = store => next => action => {
+    next(action);
+
+    const trimmedState = transformStateForHash(store.getState());
+    const base64StateHash = convertStateToBase64String(trimmedState);
+
+    window.history.pushState(trimmedState, 'currentSketch', base64StateHash);
 };
--- client/js/store.js
+++ client/js/store.js
@@ -1,36 +1,8 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import { convertStateToBase64String, instantiateApplicationStateFromPathname } from './routing';
+import { instantiateApplicationStateFromPathname } from './routing';
 import { DEFAULT_APPLICATION_STATE } from './constants/constants';
 import { reducer } from './reducers';
-
-const transformStateForHash = state => {
-    const {
-        playbackController: {
-            tempo,
-            timeSignature: {
-                beatValue,
-                beatsPerMeasure
-            }
-        },
-        sketch
-    } = state;
-
-    return {
-        beatValue,
-        beatsPerMeasure,
-        sketch,
-        tempo
-    };
-};
-
-const stateToHashMiddleware = store => next => action => {
-    next(action);
-
-    const trimmedState = transformStateForHash(store.getState());
-    const base64StateHash = convertStateToBase64String(trimmedState);
-
-    window.history.pushState(trimmedState, 'currentSketch', base64StateHash);
-};
+import { stateToHashMiddleware } from './routing';
 
 export const store = createStore(
     reducer,
